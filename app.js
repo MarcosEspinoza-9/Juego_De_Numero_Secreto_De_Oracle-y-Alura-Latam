ebec0e43f64a4f6556598c93cc523154e3e08e66
Marcos Espinoza <marcosprogram96@gmail.com>
2024-02-04 00:11:00 -0600

    Se hizo una funcion para cambiar los datos de la etiqueta "h1" y "p"

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-let titulo = document.querySelector('h1'); // CAPTURA EL VALOR DE LA VARIABLE Y LO DEVUELVE EN OBJETO
-titulo.innerHTML = 'Juego del numero secreto';
-
-
 //A todas mis etiquetas p se le asiga el texto de escoger un numero
-let parradfo = document.querySelector('p');
-parradfo.innerHTML = 'Indica un numero del uno al 10'
+//let parrafo = document.querySelector('p');
+//parrafo.innerHTML = 'Indica un numero del uno al 10'
 
-
+function asignarTextoElemento(elemento, texto){
+    let elementoHTML = document.querySelector(elemento); // CAPTURA EL VALOR DE LA VARIABLE Y LO DEVUELVE EN OBJETO
+    elementoHTML.innerHTML = texto;
+    
+}
 
 function intento () {
 alert('si funciona la funcion!')
 
 }
+
+asignarTextoElemento('h1',"Juego del numero Secreto!")
+asignarTextoElemento('p',"Indica un numero del 1 al 10")
