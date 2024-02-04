32d31f770783d50d444f0f2213f0416df35ee47a
Marcos Espinoza <marcosprogram96@gmail.com>
2024-02-04 17:44:40 -0600

    se agrego el mensaje si acerto o fallo pero sin limite de intentos

--- app.js
+++ app.js
@@ -2,16 +2,41 @@
 //let parrafo = document.querySelector('p');
 //parrafo.innerHTML = 'Indica un numero del uno al 10'
 
+let numeroSecretro = numsecreto();
+    console.log(numeroSecretro);
+
 function asignarTextoElemento(elemento, texto){
     let elementoHTML = document.querySelector(elemento); // CAPTURA EL VALOR DE LA VARIABLE Y LO DEVUELVE EN OBJETO
     elementoHTML.innerHTML = texto;
     
 }
 
-function intento () {
-alert('si funciona la funcion!')
+function Verificarintento() {
+//forma para buscar en el html por etiqueta
+//let elementoHTML = document.querySelector('intentar')
+    let elementoHTML = document.getElementById('intentoUsuario').value;
 
+console.log("Numero secreto: " , numeroSecretro);
+/*console.log("Numero ingresado",elementoHTML);
+console.log(elementoHTML==numeroSecretro);
+*/
+
+    if (elementoHTML==numeroSecretro) {
+     let adivinaste = document.querySelector('h1');
+        adivinaste.innerHTML = "¡ADIVINASTE!" 
+    } else {
+        alert('¡PERDISTE :( !')
+    }
+
+}
+
+
+//Funcion para hacer numeros aleatorios
+function numsecreto() {
+//el floor es para que tome solo numeros enteros sin decimales
+    return Math.floor(Math.random()*10+1)
 }
 
 asignarTextoElemento('h1',"Juego del numero Secreto!")
 asignarTextoElemento('p',"Indica un numero del 1 al 10")
+
