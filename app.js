56a96cffeb7d789d9ba80eae70373f3684ea8aff
Marcos Espinoza <marcosprogram96@gmail.com>
2024-02-03 22:19:03 -0600

    Creando funcion para el onclick del boton

---
+++ app.js
@@ -0,0 +1,13 @@
+let titulo = document.querySelector('h1'); // CAPTURA EL VALOR DE LA VARIABLE Y LO DEVUELVE EN OBJETO
+titulo.innerHTML = 'Juego del numero secreto';
+
+
+//A todas mis etiquetas p se le asiga el texto de escoger un numero
+let parradfo = document.querySelector('p');
+parradfo.innerHTML = 'Indica un numero del uno al 10'
+
+
+
+function intento () {
+
+}
