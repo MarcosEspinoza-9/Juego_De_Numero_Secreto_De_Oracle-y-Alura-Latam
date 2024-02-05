02112b56e29cd5b7f795cf6c3878cb5fd486dfca
Marcos Espinoza <marcosprogram96@gmail.com>
2024-02-04 21:23:25 -0600

    se agrego el operador ternario junto con el template string en la funcion de "verificarintento"

--- app.js
+++ app.js
@@ -3,47 +3,53 @@
 //parrafo.innerHTML = 'Indica un numero del uno al 10'
 
 let numeroSecretro = numsecreto();
-    console.log(numeroSecretro);
+console.log(numeroSecretro);
 
-function asignarTextoElemento(elemento, texto){
-    let elementoHTML = document.querySelector(elemento); // CAPTURA EL VALOR DE LA VARIABLE Y LO DEVUELVE EN OBJETO
-    elementoHTML.innerHTML = texto;
-    
+let intentos = 1;
+
+function asignarTextoElemento(elemento, texto) {
+  let elementoHTML = document.querySelector(elemento); // CAPTURA EL VALOR DE LA VARIABLE Y LO DEVUELVE EN OBJETO
+  elementoHTML.innerHTML = texto;
 }
 
 function Verificarintento() {
-//forma para buscar en el html por etiqueta
-//let elementoHTML = document.querySelector('intentar')
-    let elementoHTML = document.getElementById('intentoUsuario').value;
+  //forma para buscar en el html por etiqueta
+  //let elementoHTML = document.querySelector('intentar')
+  let elementoHTML = document.getElementById("intentoUsuario").value;
 
-console.log("Numero secreto: " , numeroSecretro);
-/*console.log("Numero ingresado",elementoHTML);
+  console.log("Numero secreto: ", numeroSecretro);
+  /*console.log("Numero ingresado",elementoHTML);
 console.log(elementoHTML==numeroSecretro);
 */
 
-    if (elementoHTML==numeroSecretro) {
-     let adivinaste = document.querySelector('p');
-        adivinaste.innerHTML = "¡FELICIDADES LO ADIVINASTE!" 
+  if (elementoHTML == numeroSecretro) {
+    let adivinaste = document.querySelector("p");
+    //operador ternario es cuando evaluamos en una sola linea (if,else) por ejemplo al final de la linea de abajo
+    adivinaste.innerHTML = `¡FELICIDADES LO ADIVINASTE EN ${intentos} ${(intentos === 1 ? 'VEZ' : 'VECES!')} `;
+  } else {
+    //EL USUARIO NO ACERTO
+    if (elementoHTML > numeroSecretro) {
+      asignarTextoElemento("p", "El numero secreto es menor");
     } else {
-       if (elementoHTML> numeroSecretro) {
-            asignarTextoElemento('p',"El numero secreto es menor")   
-        } else {
-            asignarTextoElemento('p',"El numero secreto es mayor")   
-        }
-       }
-       return;
+      asignarTextoElemento("p", "El numero secreto es mayor");
     }
+    intentos++;
+    limpiar();
+  }
+  return;
+}
 
 
-
+function limpiar() {
+   valorCaja = document.querySelector('#elementoHTML'). valorCaja.value = '';
+}
 
 
 //Funcion para hacer numeros aleatorios
 function numsecreto() {
-//el floor es para que tome solo numeros enteros sin decimales
-    return Math.floor(Math.random()*10+1)
+  //el floor es para que tome solo numeros enteros sin decimales
+  return Math.floor(Math.random() * 10 + 1);
 }
 
-asignarTextoElemento('h1',"Juego del numero Secreto!")
-asignarTextoElemento('p',"Indica un numero del 1 al 10")
-
+asignarTextoElemento("h1", "Juego del numero Secreto!");
+asignarTextoElemento("p", "Indica un numero del 1 al 10");
